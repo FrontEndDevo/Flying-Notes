ecb7a0436ec6fe8c2a59c8552b290d41f876a9ab
FrontEndDevo <mylinkacco@gmail.com>
2022-10-13 18:46:15 +0200

    Change input events handlers (onFocus & onBlur) from using useState to useReducer

--- src/components/Auth/Auth.jsx
+++ src/components/Auth/Auth.jsx
@@ -1,31 +1,73 @@
-import React, { useState } from "react";
+import React, { useReducer } from "react";
 import classes from "./Auth.module.scss";
 import auth_img from "../../assets/images/auth_img.svg";
 import avatar from "../../assets/images/avatar.png";
+
+// Initial State for input events:
+const initialState = {
+  usernameBorder: false,
+  passwordBorder: false,
+  usernameLabel: false,
+  passwordLabel: false,
+};
+
+// Username & Password events
+const inputReducer = (state, action) => {
+  switch (action.event) {
+    case "USERNAME_FOCUS":
+      return {
+        ...state,
+        usernameBorder: true,
+      };
+    case "USERNAME_BLUR":
+      return {
+        ...state,
+        usernameBorder: false,
+      };
+
+    case "PASSWORD_FOCUS":
+      return {
+        ...state,
+        passwordBorder: true,
+      };
+    case "PASSWORD_BLUR":
+      return {
+        ...state,
+        passwordBorder: false,
+      };
+
+    default:
+      return initialState;
+  }
+};
+
 const Auth = () => {
+  const [events, dispatch] = useReducer(inputReducer, initialState);
+
   // Username (onFocus & onBlur) events handlers:
-  const [isUsernameFocused, setIsUsernameFocused] = useState(false);
   const usernameFocusHandler = () => {
-    setIsUsernameFocused(true);
+    dispatch({ event: "USERNAME_FOCUS" });
   };
   const usernameBlurHandler = () => {
-    setIsUsernameFocused(false);
+    dispatch({ event: "USERNAME_BLUR" });
   };
+
   // Password (onFocus & onBlur) events handlers:
-  const [isPasswordFocused, setIsPasswordFocused] = useState(false);
   const passwordFocusHandler = () => {
-    setIsPasswordFocused(true);
+    dispatch({ event: "PASSWORD_FOCUS" });
   };
   const passwordBlurHandler = () => {
-    setIsPasswordFocused(false);
+    dispatch({ event: "PASSWORD_BLUR" });
   };
 
+  // Set username classes:
   const usernameFocus = `${classes.field} ${
-    isUsernameFocused ? classes.focus : ""
+    events.usernameBorder ? `${classes.borders} ${classes.labels}` : ""
   }`;
 
+  // Set password classes:
   const passwordFocus = `${classes.field} ${
-    isPasswordFocused ? classes.focus : ""
+    events.passwordBorder ? `${classes.borders} ${classes.labels}` : ""
   }`;
 
   return (
