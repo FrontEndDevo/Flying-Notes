b5e6c6e86b74084d426abbff1997559cc4c48c76
FrontEndDevo <mylinkacco@gmail.com>
2022-10-13 18:06:09 +0200

    Finish input events handlers (onFocus & onBlur) for ::before

--- src/components/Auth/Auth.jsx
+++ src/components/Auth/Auth.jsx
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import classes from "./Auth.module.scss";
 import auth_img from "../../assets/images/auth_img.svg";
+import avatar from "../../assets/images/avatar.png";
 const Auth = () => {
+  // Username (onFocus & onBlur) events handlers:
+  const [isUsernameFocused, setIsUsernameFocused] = useState(false);
+  const usernameFocusHandler = () => {
+    setIsUsernameFocused(true);
+  };
+  const usernameBlurHandler = () => {
+    setIsUsernameFocused(false);
+  };
+  // Password (onFocus & onBlur) events handlers:
+  const [isPasswordFocused, setIsPasswordFocused] = useState(false);
+  const passwordFocusHandler = () => {
+    setIsPasswordFocused(true);
+  };
+  const passwordBlurHandler = () => {
+    setIsPasswordFocused(false);
+  };
+
+  const usernameFocus = `${classes.field} ${
+    isUsernameFocused ? classes.focus : ""
+  }`;
+
+  const passwordFocus = `${classes.field} ${
+    isPasswordFocused ? classes.focus : ""
+  }`;
+
   return (
     <section className={classes.auth}>
       <div className={classes["wave-background"]}>
@@ -9,19 +35,29 @@ const Auth = () => {
       </div>
       <div className={classes.content}>
         <div className={classes.user}>
-          <img src="" alt="" />
+          <img src={avatar} alt="auth-avatar" />
           <h3>Welcome</h3>
         </div>
         <form>
-          <div className={classes.field}>
-            <label htmlFor="">Username</label>
-            <i>Icon</i>
-            <input type="text" name="" id="" />
+          <div className={usernameFocus}>
+            <label htmlFor="username">Username</label>
+            <input
+              type="text"
+              name="username"
+              id="username"
+              onFocus={usernameFocusHandler}
+              onBlur={usernameBlurHandler}
+            />
           </div>
-          <div className={classes.field}>
-            <label htmlFor="">Password</label>
-            <i>Icon</i>
-            <input type="text" name="" id="" />
+          <div className={passwordFocus}>
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              name="password"
+              id="password"
+              onFocus={passwordFocusHandler}
+              onBlur={passwordBlurHandler}
+            />
           </div>
           <button>Login</button>
         </form>
