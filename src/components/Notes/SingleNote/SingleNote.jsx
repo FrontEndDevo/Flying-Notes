d2b7b6ef90684f75c68c58179cc43dc9418fa4f2
FrontEndDevo <mylinkacco@gmail.com>
2022-10-16 23:49:20 +0200

    Edit the note button

--- src/components/Notes/SingleNote/SingleNote.jsx
+++ src/components/Notes/SingleNote/SingleNote.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import classes from "./SingleNote.module.scss";
+import editNote from "../../../assets/images/edit.png";
+
 const SingleNote = (props) => {
   // A state to handle (confirm message) when delete a note.
   const [isHover, setIsHover] = useState(false);
@@ -24,7 +26,12 @@ const SingleNote = (props) => {
         <p>{props.content}</p>
       </div>
       <div className={classes["note-tools"]}>
-        <div className={classes.tools}>note tools</div>
+        <div className={classes.tools}>
+          <button>
+            <img src={editNote} alt="edit-the-note" />
+            Edit the note?
+          </button>
+        </div>
         <div className={classes.delete}>
           {isHover && <p>Double click to confirm deletion</p>}
           <button
